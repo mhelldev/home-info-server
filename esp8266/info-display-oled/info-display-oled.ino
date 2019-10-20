d216bd245b4c4a289e76f452f9ba71fe86ec670f
michael heller <mheller2@gmx.net>
2019-10-20 15:29:01 +0200

    Fix layout for waste dates

--- esp8266/info-display-oled/info-display-oled.ino
+++ esp8266/info-display-oled/info-display-oled.ino
@@ -365,8 +365,9 @@ void drawWasteDateScreen(String type, String dateSimple, String day) {
   display2.setTextAlignment(TEXT_ALIGN_LEFT);
   display2.setFont(ArialMT_Plain_24);
   display2.drawString(5, 0, type);
+  display2.setFont(ArialMT_Plain_16);
   display2.drawString(5, 25, dateSimple);
-  display2.drawString(5, 50, day);
+  display2.drawString(5, 35, day);
   display2.drawXbm(80, 10, Trash_width, Trash_height, Trash_bits);
   display2.drawVerticalLine(127, 0, 64);
 
