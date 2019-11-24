4e2be53baa124d0b8a88cba39f7a4fc0f493a89c
michael heller <mheller2@gmx.net>
2019-11-24 20:59:00 +0100

    add cors middleware

--- app.ts
+++ app.ts
@@ -5,6 +5,7 @@ import {RssEndpoints} from "./service/endpoints/RssEndpoints";
 import {MarketEndpoints} from "./service/endpoints/MarketEndpoints";
 
 let express = require('express')
+let cors = require('cors');
 let port = process.env.PORT || 3000
 let app = express()
 let weatherEndpoints = new WeatherEndpoints()
@@ -12,6 +13,8 @@ let wasteDateEndpoints = new WasteDateEndpoints()
 let trainEndpoints = new TrainEndpoints()
 let rssEndpoints = new RssEndpoints()
 let marketEndpoints = new MarketEndpoints()
+
+app.use(cors());
 
 app.get('/', function (req: any, res: any) {
     res.send(JSON.stringify({ Hello: 'Home Info Server'}))
